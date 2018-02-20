5ef1da5ad9802ccb4e6355a2f2bf90aeee0001e2
Karel Willems <willems.karel@hotmail.com>
2018-02-20 18:59:45 +0100

    added R scripts + results + images

---
+++ R/conc_ms.R
@@ -0,0 +1,13 @@
+#autobench --single_host --host1 192.168.33.100 --quiet               --low_rate 20 --high_rate 200 --rate_step 2 --num_call 1           --num_conn 50 --timeout 5 --file autobench_patched.tsv
+#autobench --single_host --host1 192.168.33.101 --quiet               --low_rate 20 --high_rate 200 --rate_step 2 --num_call 1           --num_conn 50 --timeout 5 --file autobench_vulnerable.tsv
+
+#require("ggplot2")
+
+#patched
+results.tsv <- read.table("~/Desktop/R/results_patched.tsv", header = TRUE, sep = "\t")
+myvars <- c("dem_req_rate", "resp_time_192.168.33.100")
+newdata <- results.tsv[myvars]
+
+ms <- newdata[,]
+plot(ms,  type='l', col='red', main ='Response time', xlab='Concurrent connections', ylab='ms')
+grid(col = "lightgray", lty = "dotted")
---
+++ R/ms_request.R
@@ -0,0 +1,33 @@
+#ab -n 1000 -g ab_patched.txt http://192.168.33.100/
+#ab -n 1000 -g ab_vulnerable.txt http://192.168.33.101/
+
+#require("ggplot2")
+
+
+#patched
+ab_patched.txt <- read.table("~/Desktop/R/ab_patched.txt", header = TRUE, sep = "\t")
+myvarspatched <- "ttime"
+newdatapatched <- ab_patched.txt[myvarspatched]
+
+ms_patched <- newdatapatched[,]
+plot(ms_patched,  type='l', col='red', main ='Response time patched', xlab='request', ylab='ms')
+
+#vulnerable
+ab_vulnerable.txt <- read.table("~/Desktop/R/ab_vulnerable.txt", header = TRUE, sep = "\t")
+myvarsvulnerable <- "ttime"
+newdatavulnerable <- ab_vulnerable.txt[myvarsvulnerable]
+
+ms_vulnerable <- newdatavulnerable[,]
+plot(ms_vulnerable, type='l', col='red', main ='Response time vulnerable', xlab='request', ylab='ms')
+
+
+#patched + vulnerable
+plot(ms_vulnerable, type='l', col='red', main ='Response time', xlab='request', ylab='ms')
+lines(ms_patched, type="l", col="blue")
+
+legend(200, 24,  # Plot legend
+       c("vulnerable", "patched"),
+       col = c("red", "blue"),
+       lty = 1,
+       lwd = 2)
+
